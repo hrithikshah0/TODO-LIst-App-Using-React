556b366bf7ec231b317bc4b49b9754c3d586d433
hrithikshah0 <ritikshah@himalayacollege.edu.np>
2025-09-20 21:35:13 +0545

    add basic TODO list app with form and state management
    - Implemented form with input field and save button
    - Used React useState hook to manage to-do list state
    - Prevented duplicate items with validation check
    - Displayed sample to-do item with delete placeholder (×)
    - Applied basic Tailwind CSS styling for layout and design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [toDoList, setToDoList] = useState([]);
+
+  let saveToDoList = (event) => {
+
+    let toDoName = event.target.toDoName.value;
+    if (!toDoList.includes(toDoName)) {
+      let finalToDOList = [...toDoList, toDoName];
+      setToDoList(finalToDOList);
+    }
+    else {
+      alert("Already Exist!!");
+    }
+
+    event.preventDefault();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='text-center text-[28px] font-bold'>TODO List</h1>
+      <form className='max-w-[1170px] grid grid-cols-[80%_auto] gap-5 mx-auto' onSubmit={saveToDoList}>
+        <input type='text' className='border-2 border-[black] p-1 rounded-[6px] outline-none' name='toDoName' />
+        <button className='bg-[#ccc] p-1 rounded-[6px]'>Save</button>
+      </form>
+
+      <div className='max-w-[1170px] mx-auto mt-8'>
+        <ul>
+          <li className='bg-black text-white p-1 text-[18px] mb-4 rounded'>HTML<span className='absolute right-[20px]'>&times;</span></li>
+        </ul>
+      </div>
     </div>
   );
 }
